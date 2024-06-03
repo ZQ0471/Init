51ceace2445b0d590141183edd7b1f5cbfb716e2
zhangqi <3526395109@qq.com>
2024-06-03 09:02:10 +0800

    Initial commit

---
+++ src/test/java/com/baimi/init/InitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.baimi.init;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
