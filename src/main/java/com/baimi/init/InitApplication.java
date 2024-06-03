d3ed7cc7ada35774a105407df771e4e77eb13100
zhangqi <3526395109@qq.com>
2024-06-03 13:45:02 +0800

    修改

--- src/main/java/com/baimi/init/InitApplication.java
+++ src/main/java/com/baimi/init/InitApplication.java
@@ -1,5 +1,6 @@
 package com.baimi.init;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,11 +9,11 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @MapperScan("com.baimi.init.mapper")
 @EnableSwagger2
-@EnableCaching
+//@EnableCaching
 @SpringBootApplication
 public class InitApplication {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws JsonProcessingException {
         SpringApplication.run(InitApplication.class, args);
     }
 
--- src/main/java/com/baimi/init/config/RedisConfig.java
+++ src/main/java/com/baimi/init/config/RedisConfig.java
@@ -4,19 +4,24 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.GenericToStringSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class RedisConfig {
 
     @Bean
-    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
         RedisTemplate<String, Object> template = new RedisTemplate<>();
-        template.setConnectionFactory(connectionFactory);
+        template.setConnectionFactory(redisConnectionFactory);
+
+        // 使用 StringRedisSerializer 来序列化和反序列化 key 值
         template.setKeySerializer(new StringRedisSerializer());
-        template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+        // 使用 JdkSerializationRedisSerializer 来序列化和反序列化 value 值
+        // 你也可以自定义序列化器
+        template.setValueSerializer(new GenericToStringSerializer<>(Object.class));
+
+        template.afterPropertiesSet();
         return template;
     }
 }
-
